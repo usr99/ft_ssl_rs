df228c5270f3710dc9639965162176206dac62b0
mamartin <mamartin@student.42.fr>
2023-08-06 19:16:29 +0200

    program arguments w/ clap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+#![allow(unused)]
+use ft_ssl::*;
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+struct CLI {
+	/// encryption mode (md5, sha256)
+	command: String,
+
+	/// echo STDIN to STDOUT
+	#[arg(short = 'p')]
+	echo_stdin: bool,
+	
+	/// quiet mode
+	#[arg(short)]
+	quiet: bool,
+	
+	/// reverse the format of the output
+	#[arg(short)]
+	reverse: bool,
+	
+	/// additionnal string input
+	#[arg(short)]
+	strings: Vec<String>,
+
+	/// file input
+	file: Option<String>,
+}
+
 fn main() {
-    println!("Hello, world!");
+	let cli = CLI::parse();
+
+    dbg!(&cli);
 }
