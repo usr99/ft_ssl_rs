936c5070b0437c985e4983981b9f4cf6d6f299f7
mamartin <mamartin@mail>
2024-02-22 00:21:51 +0100

    encrypt/decrypt argument

--- src/cli/base64.rs
+++ src/cli/base64.rs
@@ -1,23 +1,18 @@
 use std::io::{Read, Write};
 
-use super::{Cipher, Kind};
+use super::{Cipher, Ciphering, CipheringArg};
 
 use clap::Parser;
 
 #[derive(Debug, Parser)]
 pub struct Options {
-	/// decode mode
-	#[arg(short)]
-	decode: bool,
-	
-	/// encode mode
-	#[arg(short, default_value = "true")]
-	encode: bool,
-	
+	#[clap(flatten)]
+	ciphering: CipheringArg,
+
 	/// input file
 	#[arg(short)]
 	input_file: Option<String>,
-	
+
 	/// output file
 	#[arg(short)]
 	output_file: Option<String>
@@ -31,13 +26,17 @@ impl Cipher for Options {
 			None => std::io::stdin().read_to_end(&mut input)?
 		};
 
-		let result = match self.decode {
-			true => ft_ssl::base64::decode(&input),
-			false => ft_ssl::base64::encode(&input)
+		let result = match self.ciphering.kind {
+			Ciphering::Decrypt => ft_ssl::base64::decode(&input),
+			Ciphering::Encrypt => ft_ssl::base64::encode(&input)
 		};
 
 		match self.output_file {
-			Some(ref filename) => std::fs::File::options().write(true).create(true).open(filename)?.write_all(&result)?,
+			Some(ref filename) => std::fs::File::options()
+				.write(true)
+				.create(true)
+				.open(filename)?
+				.write_all(&result)?,
 			None => {
 				std::io::stdout().write_all(&result)?;
 				print!("\n");
--- src/cli/mod.rs
+++ src/cli/mod.rs
@@ -1,14 +1,14 @@
-mod hash;
 mod base64;
 mod des;
+mod hash;
 
+use anyhow::anyhow;
+use clap::{Command, Parser, ArgAction::SetTrue, ValueEnum};
 use std::{
 	env::ArgsOs,
 	ffi::OsString,
-	iter::{Once, Chain}
+	iter::{Chain, Once}
 };
-use clap::{Parser, Command, Arg};
-use anyhow::anyhow;
 
 pub trait Cipher {
 	fn execute(&self) -> anyhow::Result<()>;
@@ -20,20 +20,29 @@ macro_rules! parse_options {
 			<$struct_name>::try_parse_from(args)
 				.and_then(|opt| Ok(Box::new(opt) as Box<dyn Cipher>))
 		}
-	}
+	};
 }
 
-const ENCRYPTIONS: [(&str, fn(Vec<String>) -> Result<Box<dyn Cipher>, clap::error::Error>); 3] = [
+const ENCRYPTIONS: [(
+	&str,
+	fn(Vec<String>) -> Result<Box<dyn Cipher>, clap::error::Error>
+); 6] = [
 	("md5", parse_options!(hash::MD5Options)),
 	("sha256", parse_options!(hash::SHA256Options)),
 	("base64", parse_options!(base64::Options)),
+	("des", parse_options!(des::ECBOptions)),
+	("des-ecb", parse_options!(des::ECBOptions)),
+	("des-cbc", parse_options!(des::CBCOptions))
 ];
 
 pub fn parse_cipher(command: &str, args: Vec<String>) -> Option<Box<dyn Cipher>> {
 	match ENCRYPTIONS.iter().find(|(name, _)| &command == name) {
 		Some((_, parse)) => match parse(args) {
 			Ok(cipher) => Some(cipher),
-			Err(e) => { e.print(); None }
+			Err(e) => {
+				e.print();
+				None
+			}
 		},
 		None => {
 			eprintln!("bad command: {command}\n");
@@ -47,7 +56,26 @@ pub fn parse_cipher(command: &str, args: Vec<String>) -> Option<Box<dyn Cipher>>
 	}
 }
 
-enum Kind {
-	Encode,
-	Decode
+#[derive(ValueEnum, Copy, Clone, Debug)]
+enum Ciphering {
+	Encrypt,
+	Decrypt
+}
+
+#[derive(clap::Args, Copy, Clone, Debug)]
+#[group(multiple = false)]
+struct CipheringArg {
+	/// encrypt (default)
+	#[arg(short='e', overrides_with = "decrypt")]
+	encrypt: bool,
+
+	/// decrypt
+	#[arg(short='d', overrides_with = "encrypt")]
+	decrypt: bool,
+
+	#[arg(hide = true, default_value = "encrypt", default_value_ifs=[
+		("encrypt", "true", "encrypt"),
+		("decrypt", "true", "decrypt")
+	])]
+	kind: Ciphering
 }
